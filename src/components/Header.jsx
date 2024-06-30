fd0507e304f2c5fe0202d4d552b6b44ee6d79c48
fmartintbx <facundo.corvalan@mi.unc.edu.ar>
2024-06-30 15:16:40 -0300

    Detectando si tiene objetos el carrito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,10 @@ return (
                     <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
                     <div id="carrito" className="bg-white p-3">
-                        <p className="text-center">El carrito esta vacio</p>
+                        
+                        {cart.length === 0  ? (
+                          <p className="text-center">El carrito esta vacio</p>
+                        ) : (  
                         <table className="w-100 table">
                             <thead>
                                 <tr>
@@ -68,7 +71,7 @@ return (
                                  ))}
                             </tbody>
                         </table>
-
+                        )}
                         <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
                         <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                     </div>
